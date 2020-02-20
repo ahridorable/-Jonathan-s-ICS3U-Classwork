949b17396440c7f97e1a23382e2862c9f7f870d9
AkaAhri <34429582+ahridorable@users.noreply.github.com>
2020-02-20 09:21:24 -0500

    Create make_middle.py

---
+++ make_middle.py
@@ -0,0 +1,16 @@
+def make_middle(num)
+
+    a = 0
+    b = []
+    for i in num:
+        a += 1
+        if a == len(num) / 2
+            b +=num
+
+    return b
+
+
+def plus_two(num1, num2):
+    return num1 + num2
+
+def swap_end():
